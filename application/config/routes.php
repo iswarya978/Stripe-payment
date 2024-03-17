e9ec701696e40c45d2b1d35d39295713a2ab78ab
user <ishwaryajp3010@gmail.com>
2024-03-18 00:46:03 +0530

    routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'StripePayment/index';
+$route['default_controller'] = 'StripePayment/login';
 $route['stripe/callback'] = 'StripePayment/callback';
 $route['stripe/checkout/(:any)'] = 'StripePayment/checkout/$1';
 $route['stripe/success'] = 'StripePayment/success';
